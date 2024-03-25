da3379ea9dcf502789f14ea1c22e440b4ab3611a
Ahh069 <mshyt0577@gmail.com>
2024-03-25 15:47:13 -0400

    update

--- js/script.js
+++ js/script.js
@@ -13,41 +13,42 @@ fetch('https://dog.ceo/api/breeds/image/random')
   .catch(error => {
     console.error('There was a problem fetching the data:', error);
   });
+  
 
-function populateContent(data) {
-  // Assuming data is an array of movie objects
-  data.forEach(movie => {
-    // Populate your application's content with movie information
-    console.log(movie.title, movie.year);
-    // Example: You can create HTML elements dynamically and append them to the DOM
-    // const movieElement = document.createElement('div');
-    // movieElement.textContent = `${movie.title} (${movie.year})`;
-    // document.getElementById('movieList').appendChild(movieElement);
-  });
-}
+// function populateContent(data) {
+//   // Assuming data is an array of movie objects
+//   data.forEach(movie => {
+//     // Populate your application's content with movie information
+//     console.log(movie.title, movie.year);
+//     // Example: You can create HTML elements dynamically and append them to the DOM
+//     // const movieElement = document.createElement('div');
+//     // movieElement.textContent = `${movie.title} (${movie.year})`;
+//     // document.getElementById('movieList').appendChild(movieElement);
+//   });
+// }
 
 
-// Import Axios
-const axios = require('axios');
+// // Import Axios
+// const axios = require('axios');
 
-// Fetch data using Axios
-axios.get('https://api.example.com/movies')
-  .then(response => {
-    // Data is retrieved successfully, now you can populate your application's content
-    populateContent(response.data);
-  })
-  .catch(error => {
-    console.error('There was a problem fetching the data:', error);
-  });
+// // Fetch data using Axios
+// axios.get('https://api.example.com/movies')
+//   .then(response => {
+//     // Data is retrieved successfully, now you can populate your application's content
+//     populateContent(response.data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem fetching the data:', error);
+//   });
 
-function populateContent(data) {
-  // Assuming data is an array of movie objects
-  data.forEach(movie => {
-    // Populate your application's content with movie information
-    console.log(movie.title, movie.year);
-    // Example: You can create HTML elements dynamically and append them to the DOM
-    // const movieElement = document.createElement('div');
-    // movieElement.textContent = `${movie.title} (${movie.year})`;
-    // document.getElementById('movieList').appendChild(movieElement);
-  });
-}
+// function populateContent(data) {
+//   // Assuming data is an array of movie objects
+//   data.forEach(movie => {
+//     // Populate your application's content with movie information
+//     console.log(movie.title, movie.year);
+//     // Example: You can create HTML elements dynamically and append them to the DOM
+//     // const movieElement = document.createElement('div');
+//     // movieElement.textContent = `${movie.title} (${movie.year})`;
+//     // document.getElementById('movieList').appendChild(movieElement);
+//   });
+// }
