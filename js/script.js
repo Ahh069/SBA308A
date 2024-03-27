24dcff10d3aa8a6f0e983071704d1274c6d4f238
Ahh069 <mshyt0577@gmail.com>
2024-03-26 20:39:33 -0400

    update

--- js/script.js
+++ js/script.js
@@ -2,29 +2,48 @@
 console.log(document.title);
 
 // Fetching random dog images from the Dog CEO's Dog API
-fetch('https://dog.ceo/api/breeds/image/random/5')
-  .then(response => response.json())
-  .then(data => {
-    // Log the fetched data
-    console.log(data);
+document
+.getElementById("myBtn")
+.addEventListener("click", testRequest);
 
-    // Process the fetched data and populate your application's content
-    const dogImages = data.message;
-    const galleryDiv = document.getElementById('gallery');
+async function testRequest() {
+let inputVal = document.getElementById("myInput").value;
+let requestBody = { data: inputVal };
+console.log(requestBody);
 
-    // Clear previous content of the gallery
-    galleryDiv.innerHTML = '';
+const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    body: JSON.stringify(requestBody),
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+    }
+});
 
-    // Display fetched dog images in the gallery
-    dogImages.forEach(imageUrl => {
-      const img = document.createElement('img');
-      img.src = imageUrl;
-      img.alt = 'Dog';
-      galleryDiv.appendChild(img);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+alertResponse(response);
+}
 
-  
+async function alertResponse(response) {
+if (response.ok) {
+    const textData = await response.text();
+    alert(textData);
+} else {
+    alert("The request returned a status other than 200 OK: " + response.status);
+}
+}
+
+const dog = document.getElementById('dog');
+dog.addEventListener("click", getNewDog);
+
+async function getNewDog() {
+dog.style.cursor = 'wait';
+const response = await fetch("https://dog.ceo/api/breeds/image/random");
+// console.log(response);
+const jsonData = await response.json();
+// console.log(jsonData);
+const url = jsonData.message;
+
+dog.src = url;
+dog.style.cursor = 'pointer';
+}
+
+getNewDog();
