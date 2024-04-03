f3fa01554c8ac8abe8e479a744d56fed75426bb6
Ahh069 <mshyt0577@gmail.com>
2024-04-03 10:38:13 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
 // Get the title of the document
-console.log(document.title);
+console.log("in scripts");
 
-document
-        .getElementById("myBtn")
-        .addEventListener("click", testRequest);
+// document
+//         .getElementById("myBtn")
+//         .addEventListener("click", testRequest);
     
-    async function testRequest() {
-        let inputVal = document.getElementById("myInput").value;
-        let requestBody = { data: inputVal };
-        console.log(requestBody);
+//     async function testRequest() {
+//         let inputVal = document.getElementById("myInput").value;
+//         let requestBody = { data: inputVal };
+//         console.log(requestBody);
 
-        const response = await fetch('https://api.thedogapi.com/v1/images/search?limit=9&x-api-key=live_fEyaF5CcS1CK9eduBzKqxtkNjeYcbCYn3E3nF5Q8RiZn0KYRRMlFa8AbL4a333Lm', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            }
-        });
+//         const response = await fetch('https://api.thedogapi.com/v1/images/search?limit=9&x-api-key=live_fEyaF5CcS1CK9eduBzKqxtkNjeYcbCYn3E3nF5Q8RiZn0KYRRMlFa8AbL4a333Lm', {
+//             method: 'GET',
+//             body: JSON.stringify(requestBody),
+//             headers: {
+//                 'Content-Type': 'application/json; charset=UTF-8',
+//             }
+//         });
 
-        alertResponse(response);
-    }
+//         alertResponse(response);
+//     }
 
-    async function alertResponse(response) {
-        if (response.ok) {
-            const textData = await response.text();
-            alert(textData);
-        } else {
-            alert("The request returned a status other than 200 OK: " + response.status);
-        }
-    }
+    // async function alertResponse(response) {
+    //     if (response.ok) {
+    //         const textData = await response.text();
+    //         alert(textData);
+    //     } else {
+    //         alert("The request returned a status other than 200 OK: " + response.status);
+    //     }
+    // }
 
     const dog = document.getElementById('dog');
     dog.addEventListener("click", getNewDog);
 
     async function getNewDog() {
         dog.style.cursor = 'wait';
-        const response = await fetch("https://https://cdn2.thedogapi.com/images/iIgf9U3bv.jpgdog.ceo/api/breeds/image/random");
-        // console.log(response);
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        console.log(response);
         const jsonData = await response.json();
         // console.log(jsonData);
         const url = jsonData.message;
